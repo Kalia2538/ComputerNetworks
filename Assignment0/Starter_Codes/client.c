aa63e8b2a997e61187d47bf5b1b9ee0f1969f478
Kalia <Kalia2538@gmail.com>
2025-02-06 00:06:20 -0500

    worksssss

--- Assignment0/Starter_Codes/client.c
+++ Assignment0/Starter_Codes/client.c
@@ -17,21 +17,41 @@
  * Return 0 on success, non-zero on failure
 */
 int client(char *server_ip, char *server_port) {
+  printf("made it to client function \n");
   struct sockaddr_in sin;
   int s;
   char buff[SEND_BUFFER_SIZE];
   sin.sin_family = PF_INET;
-  sin.sin_port = server_port;
-  sin.sin_addr.s_addr = INADDR_ANY; // FIGURE OUT WHAT THIS MEANS
+  int portnum = atoi(server_port);
+  sin.sin_port = portnum;
+  sin.sin_addr.s_addr = atoi(server_ip); // FIGURE OUT WHAT THIS MEANS
   // open socket
-  int sockfd = socket(PF_INET, SOCK_STREAM, PF_UNSPEC); // DC PF_UNSPEC
+  int sockfd = socket(PF_INET, SOCK_STREAM, 0); // DC PF_UNSPEC
+  printf("opened the socket \n");
+  printf("error = -1\n");
+  // char str1[20]; // Ensure it has enough space
+  printf("value = ");
+  printf("%d", sockfd);
+  printf(" end\n");
+  // printf(sockfd);
   // active open
   int con_estab = connect(sockfd, (struct sockaddr *) &sin, sizeof(sin) );
-
+  printf("success = 0, fail = -1\n");
+  // char str[20]; // Ensure it has enough space
+  printf("%d", con_estab);
+  // printf(con_estab);
+  printf("\n");
+  int x = 0;
+  // printf(fgets(buff, sizeof(buff), stdin));
   while (fgets(buff, sizeof(buff), stdin)) {
+    x++;
     buff[SEND_BUFFER_SIZE-1] = '\0';
     int len = strlen(buff) + 1;
-    send(s, buff, len, 0);
+    printf(buff);
+    int val = send(s, buff, len, 0);
+    printf("num of sent bytes: ");
+    printf("%d", val);
+    printf("\n");
   }
 
   // loop - while we haven't reached an eof
@@ -54,6 +74,12 @@ int main(int argc, char **argv) {
   }
 
   server_ip = argv[1];
+  printf("server ip: ");
+  printf(argv[1]);
+  printf("\n");
   server_port = argv[2];
+  printf("server port: ");
+  printf(argv[2]);
+  printf("\n");
   return client(server_ip, server_port);
 }
--- Assignment0/Starter_Codes/server.c
+++ Assignment0/Starter_Codes/server.c
@@ -26,26 +26,66 @@ struct socket_info
  * Return 0 on success, non-zero on failure
 */
 int server(char *server_port) {
-    char buff[RECV_BUFFER_SIZE];
-    int s, new_s, buff_len;
-    socklen_t addr_len;
-    // struct addrinfo hints;
-    struct sockaddr_in sin;
-    sin.sin_family = PF_INET;
-    int portnum = atoi(server_port);
-    sin.sin_port = portnum;
-    sin.sin_addr.s_addr = INADDR_ANY; // FIGURE OUT WHAT THIS MEANS
-    int sockfd = socket(PF_INET, SOCK_STREAM, PF_UNSPEC); // DC PF_UNSPEC
+  printf("in the server function\n");
+  char buff[RECV_BUFFER_SIZE];
+  int s, new_s, buff_len;
+  socklen_t addr_len;
+  // struct addrinfo hints;
+  struct sockaddr_in sin;
+  // struct getaddr
+  sin.sin_family = PF_INET;
+  int portnum = atoi(server_port);
+  printf("after atoi - port \n");
+  sin.sin_port = portnum;
+  printf("after sin_port \n");
+  sin.sin_addr.s_addr = atoi("127.9.0.1"); // FIGURE OUT WHAT THIS MEANS
+  printf("after ip -- s_addr \n");
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0); // DC PF_UNSPEC
+  printf("opened the socket \n");
+  printf("error = -1\n");
+  // char str1[20]; // Ensure it has enough space
+  printf("value = ");
+  printf("%d", sockfd);
+  printf(" end\n");
 
-    int success = bind(sockfd, (struct sockaddr *) &sin, sizeof(sin) );
-
-    listen(sockfd, QUEUE_LENGTH);
+  int success = bind(sockfd, (struct sockaddr *) &sin, sizeof(sin) );
+  printf("bound the socket \n");
+  printf("error = -1\n");
+  // char str1[20]; // Ensure it has enough space
+  printf("value = ");
+  printf("%d", success);
+  printf(" end\n");
+  success = listen(sockfd, QUEUE_LENGTH);
+  printf("successful listen \n");
+  printf("error = -1\n");
+  // char str1[20]; // Ensure it has enough space
+  printf("value = ");
+  printf("%d", success);
+  printf(" end\n");
   while (1)
   {
-    new_s = accept(s, (struct sockaddr *) &sin, &addr_len);
-    while (buff_len = recv(new_s, buff, sizeof(buff), 0))
+    new_s = accept(sockfd, (struct sockaddr *) &sin, &addr_len);
+    printf("accepted a client\n");
+    printf("%d", new_s);
+    // while (buff_len = recv(new_s, buff, sizeof(buff), 0))
+    // {
+    //   printf("%s", buff);
+    //   // fputs(buff, stdout);
+    // }
+    buff_len = recv(new_s, buff, sizeof(buff), 0);
+    printf("\n new first buff_len ");
+    printf("%d\n", buff_len);
+    printf("this is buff: ");
+    printf("%s", buff);
+    int abc = 1;
+    while (abc)
     {
-      fputs(buff, stdout);
+      fflush(stdout);
+      printf("%s", buff);
+      fflush(stdout);
+
+      // fputs(buff, stdout);
+      abc = recv(new_s, buff, sizeof(buff), 0);
     }
     close(new_s);
   }
@@ -59,6 +99,8 @@ int server(char *server_port) {
  * Parse command-line arguments and call server function
 */
 int main(int argc, char **argv) {
+  printf("start of main\n");
+  printf("%d", argc);
   char *server_port;
 
   if (argc != 2) {
@@ -67,5 +109,8 @@ int main(int argc, char **argv) {
   }
 
   server_port = argv[1];
+  printf("this is the server port num \n");
+  printf(server_port);
+  printf("\n");
   return server(server_port);
 }
