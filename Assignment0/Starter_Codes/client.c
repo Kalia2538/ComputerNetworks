7044583807ccdf68891802aa0ab7377359875eba
Kalia <Kalia2538@gmail.com>
2025-02-07 12:58:58 -0500

    cleaned up the comments

--- Assignment0/Starter_Codes/client.c
+++ Assignment0/Starter_Codes/client.c
@@ -17,58 +17,52 @@
  * Return 0 on success, non-zero on failure
 */
 int client(char *server_ip, char *server_port) {
-  struct sockaddr_in sin;
+
   int s;
   char buff[SEND_BUFFER_SIZE];
-  // sin.sin_family = AF_INET;
-  // int portnum = atoi(server_port);
-  // sin.sin_port = portnum;
-  // sin.sin_addr.s_addr = atoi(server_ip); // FIGURE OUT WHAT THIS MEANS
-  // // open socket
-  // int sockfd = socket(AF_INET, SOCK_STREAM, 0); // DC PF_UNSPEC
-  ////////
+
   int sockfd, numbytes;  
-    char buf[SEND_BUFFER_SIZE];
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    memset(&hints, 0, sizeof (hints));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+  char buf[SEND_BUFFER_SIZE];
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  memset(&hints, 0, sizeof (hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
 
-    if ((rv = getaddrinfo(server_ip, server_port, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+  if ((rv = getaddrinfo(server_ip, server_port, &hints, &servinfo)) != 0) {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+      return 1;
+  }
+
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+      p->ai_protocol)) == -1) {
+      perror("client: socket");
+      continue;
     }
 
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
-            perror("client: socket");
-            continue;
-        }
-
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("client: connect");
-            continue;
-        }
-
-        break;
-
-        if (p == NULL) {
-            fprintf(stderr, "client: failed to connect\n");
-            return 2;
-        }
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+        perror("client: connect");
+          continue;
     }
-    int x;
-    while ((x = fread(buff, 1, SEND_BUFFER_SIZE, stdin)) > 0) {
-      int val = send(sockfd, buff, x, 0);
-      if(val < 1) {
-        perror("simplex-talk: send");
-      }
-    
+
+    break;
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
     }
+  }
+
+  int x;
+  while ((x = fread(buff, 1, SEND_BUFFER_SIZE, stdin)) > 0) {
+    int val = send(sockfd, buff, x, 0);
+    if(val < 1) {
+      perror("simplex-talk: send");
+    }  
+  }
+
   close(sockfd);
   return 0;
 }
@@ -86,14 +80,8 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
-  server_ip = argv[1];
-  // printf("server ip: ");
-  // printf(argv[1]);
-  // printf("\n");
+  server_ip = argv[1];  
   server_port = argv[2];
-  // printf("server port: ");
-  // printf(argv[2]);
-  // printf("\n");
   fflush(stdout);
 
   return client(server_ip, server_port);
