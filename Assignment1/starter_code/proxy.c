d56065c212cfd242364689d6459b834ac26de79d
Kalia <Kalia2538@gmail.com>
2025-02-27 00:21:45 -0500

    updated proxy to parse a request and put in struct - needs testing

--- Assignment1/starter_code/proxy.c
+++ Assignment1/starter_code/proxy.c
@@ -19,9 +19,99 @@
  * Send data to the client
  * Return 0 on success, non-zero on failure
 */
+
+#define DEFAULT_NHDRS 8
+#define MAX_REQ_LEN 65535
+#define MIN_REQ_LEN 4#define DEFAULT_NHDRS 8
+#define MAX_REQ_LEN 65535
+#define MIN_REQ_LEN 4
+
+
+
 int proxy(char *proxy_port) {
+  struct addrinfo hints;
+  struct addrinfo *servinfo, *p;
+  struct sockaddr_storage their_addr; // connector's address information
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  
+  int rv;
+  if ((rv = getaddrinfo("127.0.0.1", proxy_port, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  int sockfd;
+  int yes = 1;
+
+  // establishing socket connection
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+      p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
+    }
+
+    break;
+  }
+
+  // listening for client connections
+  if (listen(sockfd, 10) == -1) { // QUESTION: what should our queue length be?
+    perror("listen");
+    exit(1);
+  }
+
+  socklen_t sin_size;
+  int new_fd;
+  while(1) {  // main accept() loop
+    sin_size = sizeof(their_addr);
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_fd == -1) {
+      perror("accept");
+      continue;
+    }
+
+    // start the forking process here
+    // for now, we will only do one conection at a time
+
+    struct ParsedRequest* parsedReq = ParsedRequest_create();
+    char buff[MAX_REQ_LEN];
+    int buffLen;
+    int val; 
+    
+    while ((val = ParsedRequest_parse(parsedReq, buff, buffLen)) >0 ) {
+      continue; // double check this
+    }
+
+    if (val == -1) {
+      // bad request
+      return -1;
+    }
+
+    // should have a fully parsed request by here
+
+    
+
+  }
   return 0;
 }
+
+
+
 
 
 int main(int argc, char * argv[]) {
