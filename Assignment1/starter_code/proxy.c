525c4f0616e1e0787ac29e78642e9b306ad5d5bf
Kalia <Kalia2538@gmail.com>
2025-03-01 17:44:27 -0500

    really cleaned up the file

--- Assignment1/starter_code/proxy.c
+++ Assignment1/starter_code/proxy.c
@@ -28,8 +28,7 @@
 
 int proxy(char *proxy_port) {
   struct sockaddr_in server;
-  int sockfd, newfd;
-  int proc_num = 0;
+  int sockfd;
 
   socklen_t x = sizeof(server);
   memset(&server, 0, sizeof(server));
@@ -37,112 +36,125 @@ int proxy(char *proxy_port) {
   server.sin_addr.s_addr = INADDR_ANY;
   server.sin_port = htons(atoi(proxy_port));
 
-  
-  sockfd = socket(AF_INET, SOCK_STREAM,0);
-  // establishing socket connection
-  if (sockfd <0) {
-      perror("server: socket");
-      return -1;
+  // creating the socket
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("proxy: socket not created\n");
+    printf("socket failed with error: %s\n", strerror(errno));
+    return -1;
   }
-  printf("sockfd = %d\n", sockfd);
+  printf("sockfd is: %d\n", sockfd);
 
-
-  
-  if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+  // binding address to the socket
+  int bind_val;
+  if ((bind_val = bind(sockfd, (struct sockaddr *)&server, sizeof(server)))< 0) {
+    perror("proxy: bind unsuccesful\n");
     printf("Bind failed with error: %s\n", strerror(errno));
-    perror("server: bind");
     close(sockfd);
     return -1;
-}
-  printf("made it to bind\n");
+  }
+  printf("bind successful with return value %d\n",bind_val);
 
   // listening for client connections
-  int l = -91;
-  if (((l = listen(sockfd, 10)) == -1)) { // QUESTION: what should our queue length be?
-    perror("listen");
+  int listen_val;
+  if (((listen_val = listen(sockfd, 10)) < 0)) { // QUESTION: what should our queue length be?
+    perror("proxy: listen unsuccessful");
+    printf("listen failed with error: %s\n", strerror(errno));
+    close(sockfd);
     return -1;
   }
-  printf("after listen\n");
+  printf("listen successful with value %d\n", listen_val);
   
 
-  // socklen_t sin_size;
   int new_fd; // fd for accepted client
   while(1) {    
     if((new_fd = accept(sockfd, (struct sockaddr *)&server, &x)) < 0) {
-      perror("accept: ");
+      perror("proxy/client: accept failed");
+      printf("accept failed with error: %s\n", strerror(errno));
+      close(sockfd);
       return -1;
     }
-    printf("this is new_fd: %d\n", new_fd);
-    printf("connected to the client\n");
-
+    printf("accept successful with new_fd = %d\n", new_fd);
+    
     int pid = fork();
     if (pid == 0) { // child process
       printf("in child process\n");
       struct ParsedRequest* parsedReq = ParsedRequest_create();
       char buff[MAX_REQ_LEN];
-      int val = -91;
-      int abc = -1;
-      char wholeReq[MAX_REQ_LEN] = ""; // double check this formatting
+      
+      int recv_bytes = 0;
       int total_bytes = 0;
-
-      int completedMessage = 0;
       int found = 0;
       // run until we recieve a message
       while (total_bytes < MAX_REQ_LEN) { // continuously recieve data from client --- then check for \r\n\r\n
-        abc = recv(new_fd, buff+total_bytes, MAX_REQ_LEN - total_bytes, 0);
+        recv_bytes = recv(new_fd, buff+total_bytes, MAX_REQ_LEN - total_bytes, 0);
         printf("this is buff: %s", buff);
-        total_bytes += abc;
+        total_bytes += recv_bytes;
         if (strstr(buff, "\r\n")) {
           printf("found ending!\n");
+          found = 1;
           break;
         }
       }
+      if (found == 0) {
+        perror("request is too large\n");
+        ParsedRequest_destroy(parsedReq);
+        close(sockfd);
+        return -1;
+      }
+
       // continue;
-      printf("this is buff again%s\n", buff);
-      memcpy(buff+abc, "\r\n", 2);
-      val = ParsedRequest_parse(parsedReq, buff, abc + 2);
+      printf("message completed%s\n", buff);
+      // add the ending
+      memcpy(buff+total_bytes, "\r\n", 2);
+
+      if ((ParsedRequest_parse(parsedReq, buff, total_bytes + 2)) < 0) {
+        perror("unsuccessful parse\n");
+        ParsedRequest_destroy(parsedReq);
+        close(sockfd);
+        return -1;
+      }
+
       if(ParsedHeader_set(parsedReq, "Connection", "close") != 0) {
         printf("issue with connection header\n");
+        ParsedRequest_destroy(parsedReq);
+        close(sockfd);
         return -1;
       }
       if (ParsedHeader_set(parsedReq, "Host", parsedReq->host) != 0) {
         printf("issue with host header\n");
+        ParsedRequest_destroy(parsedReq);
+        close(sockfd);
         return -1;
       }
 
       printf("parsed the headers\n");
       
+      // checking for default port number
       char *def_port = "80";
       if (parsedReq->port == NULL) {
         parsedReq->port = def_port;
       }
-      
-      if (val == -1) {
-        printf("bad request");
-        return -1;
-      }      
+          
 
-      // struct addrinfo hints2;
-      // struct addrinfo *servinfo2, *p2;
-      // struct sockaddr_storage their_addr2; // connector's address information
-
-      // memset(&hints2, 0, sizeof(hints2));
-      // hints2.ai_family = AF_UNSPEC;
-      // hints2.ai_socktype = SOCK_STREAM;
-      // int sockfd2;
-      ///////
       struct hostent *h = gethostbyname(parsedReq->host); // error check here
+      if (h == NULL) {
+        herror("gethostbyname - error");
+        ParsedRequest_destroy(parsedReq);
+        close(sockfd);
+        return -1;  // Return error if resolution fails
+      }
 
       struct sockaddr_in server_ad;
-      // socklen_t x2 = sizeof(client);
       memset(&server_ad, 0, sizeof(server_ad));
       server_ad.sin_family = AF_INET;
       memcpy(&server_ad.sin_addr.s_addr, h->h_addr, h->h_length);
 
       int sockfd2;
-      sockfd2 = socket(AF_INET, SOCK_STREAM, 0);
-      if (sockfd2 < 0) {
-        perror("client: socket");
+      if ((sockfd2 = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("proxy/server: socket not created\n");
+        printf("socket failed with error: %s\n", strerror(errno));
+        ParsedRequest_destroy(parsedReq);
+        close(sockfd);
         return -1;
       }
       printf("this is sockfd2: %d\n", sockfd2);
@@ -153,90 +165,60 @@ int proxy(char *proxy_port) {
       
       int num;
       if ((num = connect(sockfd2, (struct sockaddr *) &server_ad, sizeof(server_ad))) < 0) {
-        printf("%d\n", num);
+        perror("proxy/server: connection unsuccessful\n");
+        printf("connect failed with error: %s\n", strerror(errno));
+        ParsedRequest_destroy(parsedReq);
+        close(sockfd);
         close(sockfd2);
-        perror("client: connect");
-        fflush(stdout);
         return -1;
       }
-
       printf("connect succeeded\n");
-
-
-      
-
-
-      
-      // int rv2;
-      // if ((rv2 = getaddrinfo(parsedReq->host, parsedReq->port, &hints2, &servinfo2)) != 0) { //update thissss
-      //   fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv2));
-      //   return 1;
-      // }
-      // printf("rv2\n");
-      
-      // establishing connection to the server
-
-      // if ((sockfd2 = socket(p2->ai_family, p2->ai_socktype,
-      //   p2->ai_protocol)) == -1) {
-      //   perror("client: socket");
-      //   continue;
-      // }
-
-      // if (connect(sockfd2, p2->ai_addr, p2->ai_addrlen) == -1) {
-      //     close(sockfd2);
-      //       perror("client: connect");
-      //         continue;
-      //}
       
       char unBuff[MAX_REQ_LEN];
-      size_t unBuff_len =ParsedRequest_totalLen(parsedReq);
-      ParsedRequest_unparse(parsedReq, unBuff, unBuff_len);
+      size_t unBuff_len = ParsedRequest_totalLen(parsedReq);
+      int unp = ParsedRequest_unparse(parsedReq, unBuff, unBuff_len);
+      if (unp < 0) {
+        perror("unsuccessful unparse\n");
+        ParsedRequest_destroy(parsedReq);
+        close(sockfd);
+        close(sockfd2);
+
+      }
       printf("successful unparse\n");
       ParsedRequest_destroy(parsedReq);
-      int a = send(sockfd2, unBuff, unBuff_len, 0); // send request to the server
-      if (a == -1) {
+
+      // sending request to the server
+      int num_bytes;
+      if ((num_bytes = (send(sockfd2, unBuff, unBuff_len, 0))) < 0) {
         printf("error with sending request");
+        close(sockfd);
+        close(sockfd2);
         return -1;
       }
-  
-      // printf("connected with the server\n");
-      
-      // char unBuff[MAX_REQ_LEN];
-      // size_t unBuff_len =ParsedRequest_totalLen(parsedReq);
-      // ParsedRequest_unparse(parsedReq, unBuff, unBuff_len);
-      // // printf("successful unparse\n");
-      // ParsedRequest_destroy(parsedReq);
-      // int a = send(sockfd2, unBuff, unBuff_len, 0); // send request to the server
-      // if (a == -1) {
-      //   printf("error with sending request");
-      // }
-      
 
       // wait for response // just recieve everything continuously ... look at last assignment
       int recd;
       char servbuff[MAX_REQ_LEN] = "";
       while (1) { // continuously recieve data and search for buff (w/i max length)
         recd = recv(sockfd2, servbuff, MAX_REQ_LEN, 0);
-        if (recd <= 0) {
-          // printf(" this is buff: %s\n",servbuff);
+        if (recd < 0) {
+          perror("server/proxy: error receiving data\n");
+          printf("recv failed with error: %s\n", strerror(errno));
+          close(sockfd);
+          close(sockfd2);
           return -1;
+        } else if (recd == 0) {
+          break; // no more data to send
         }
         send(new_fd, servbuff, recd, 0);
       }
 
-      // printf("recieved from server\n");
-
-
-      // close connection to server (not sure if necessary for this assignment)'
-      close(sockfd2);
-      close(sockfd2);
-      
-      // send response to the client
-      // int w = send(new_fd, servbuff, recd, 0);   
-      // printf("sent to the client :)\n");  
+      close(sockfd);
+      close(sockfd2); 
       exit(pid);
     
     }   
+    close(sockfd);
 
   }
   return 0;
